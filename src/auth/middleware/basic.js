ac46f30decf3d2fe1f0942a06fc18c42b32ca70c
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-07-04 18:02:07 +0000

    Lab07 is complete

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index.js');
+
+module.exports = async (req, res, next) => {
+
+  if (!req.headers.authorization) { return _authError(); }
+
+  let basic = req.headers.authorization.split(' ').pop();
+  let [username, pass] = base64.decode(basic).split(':');
+
+  try {
+    const SIUser = await users.authenticateBasic(username, pass);
+    req.user = SIUser;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(403).send('Invalid Login');
+  }
+
+}
